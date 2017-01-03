97a625593981bafa3d056706994abc267baa9ce2
ChristianBreitkreutz <cbreitkreutz@epages.com>
2017-01-03 09:17:29 +0100

    repoertssssss

--- AddTestRulesToAllMakefiles.sh
+++ AddTestRulesToAllMakefiles.sh
@@ -3,7 +3,7 @@ ls -d */ | cut -f1 -d'/' | while read foldername
 do
   if [ -a $foldername/$filename ];then
     pattern="^test.*:.*makefile$"
-    replacestring="test unit_test integration_test regression_test test_coverage collect_coverage coverage_report critic_report sonar_report sonar_properties sonar_scan: makefile"
+    replacestring="test unit_test integration_test regression_test test_coverage collect_coverage coverage_report critic_report sonar_reports sonar_properties sonar_scan: makefile"
     if ! grep --quiet "$replacestring" $foldername/$filename; then
         echo "Changed $foldername/Makefile.PL."
     	sed -i -e "s/$pattern/$replacestring/" $foldername/$filename
